6190f7e3f6a26e674dfcc66235555a7125808a45
Ralence <ranko.ostojic@gmail.com>
2019-02-04 14:24:13 +0800

    Version 1.0.

--- README.md
+++ README.md
@@ -1,20 +1,33 @@
 # Classic Arcade Game Clone Project
 
-## Table of Contents
+## Information 
 
-- [Instructions](#instructions)
-- [Contributing](#contributing)
+* Project .........Classic Arcade Game
+* Purpose .........Udacity Front-End Nanodegree Program
+                   Project 3
+* Creator .........Ranko Ostojic
+* Created .........February, 2019
 
-## Instructions
+## JS Code Info and Structure
 
-Use this [rubric](https://review.udacity.com/#!/rubrics/15/view) for self-checking your submission.
+* Code was written in pure JS using OOP and ES6 class pattern
 
-Make sure the functions you write are **object-oriented** - either class functions (like `Player` and `Enemy`) or class prototype functions such as `Enemy.prototype.checkCollisions`. Also make sure that the keyword `this` is used appropriately within your class and class prototype functions to refer to the object the function is called upon.
+## Basic Info About The Game Functionality
 
-Your **README.md** file should be updated with instructions on both how to 1. Run and 2. Play your arcade game.
+* To start the game clone the repository to your local machine and open index.html in your browser
+* The game will start onload
+* The enemy bugs will appear in random order and with random speeds, crossing the canvas from left to right
+* The player will appear at the starting position on the grass
+* The objective of the game is to cross the road and get to the water without touching the enemy bugs
+* Use arrow keys on your keyboard to move the player around
+* If the player touches an enemy bug he will be sent to the starting position to start over
+* If the player reaches the wather the game is won and he will be sent to the starting position to start over 
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
 
-## Contributing
+##Dependencies
 
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
+* The starter code for this project was downloaded form
+  The Arcade Game project repository on GitHub:
+  https://github.com/udacity/frontend-nanodegree-arcade-game
+
+* Other than this everything was coded from scratch and has no external dependencies
--- js/app.js
+++ js/app.js
@@ -1,37 +1,97 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+class Enemy {
+    // Enemy constructor
+    constructor() {
+        // Variables applied to each of our instances go here,
+        // We've provided one for you to get started
+        this.speedRange = [200, 300, 300, 500, 500];  // The array is used to randomize the enemy's speed
+        this.speed = this.speedRange[Math.floor(Math.random() * this.speedRange.length)]; // Asigns the random speed
+        this.rangeY = [73, 156, 239]; // Array is used to randomize the row along which the enemy will move
+        this.rangeX = [-100, -150, -200, -250, -300, -350]; // Array is used to randomize the delay when enemy reapears on canvas by changing it's initial position 
+        this.x = this.rangeX[Math.floor(Math.random() * this.rangeX.length)]; // Asigns random delay and distance between enemy instances
+        this.y = this.rangeY[Math.floor(Math.random() * this.rangeY.length)]; // Asigns enemies to random rows
+        // The image/sprite for our enemies, this uses
+        // A helper we've provided to easily load images
+        this.sprite = 'images/enemy-bug.png';
+    }
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+    // Update the enemy's position, required method for game
+    // Parameter: dt, a time delta between ticks
+    update(dt) {
+        // You should multiply any movement by the dt parameter
+        // which will ensure the game runs at the same speed for
+        // all computers.
+        this.x > 500 ? this.reRender() : this.x += this.speed * dt;
+    }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
+    // Draw the enemy on the screen, required method for game
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+    // This method will reposition the enemy to the left side of the canvas
+    // and asign it to a random row, give it a random speed 
+    // and random delay before reappearing on canvas
+    reRender() {
+        this.x = this.rangeX[Math.floor(Math.random() * this.rangeX.length)];
+        this.y = this.rangeY[Math.floor(Math.random() * this.rangeY.length)];
+        this.speed = this.speedRange[Math.floor(Math.random() * this.speedRange.length)];
+    }
+}
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// Player class
+class Player {
+    // Player constructor loads the palyer image and positions it on the canvas
+    constructor() {
+        this.x = 200;
+        this.y = 405;
+        this.sprite = 'images/char-boy.png';
+    }
 
+    // Method checks if the player is colliding with any of the enemy instances
+    update() {
+        // Loops over allEnemies array to check for collisions
+        for (let enemy of allEnemies) {
+            if (this.y === enemy.y && (enemy.x + 70 >= this.x && enemy.x - 50 <= this.x)) {
+                // If the objects are touching send the player to the starting position
+                this.reposition()
+                }
+        }
+    }
+
+    // Sends the player back to the starting position
+    reposition() {
+        this.x = 200;
+        this.y = 405;
+    }
+
+    // Hendles the input and updates the player's position
+    handleInput(key) {
+        if (key === 'up') {
+            this.y -= 83;
+            // If the wather is reached the game is won
+            this.y === -10 ? this.reposition() : this.y;
+        } else if (key === 'down') {
+            this.y = this.y === 405 ? this.y : this.y += 83;
+        } else if (key === 'right') {
+            this.x = this.x === 400 ? this.x : this.x += 100;
+        } else {
+            this.x = this.x === 0 ? this.x : this.x -= 100;
+        }
+    }
+
+    // Renders the player figure on the canvas
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y); 
+    }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+// Creates the enemy instances and stores them in the array
+const allEnemies = [new Enemy(), new Enemy(), new Enemy(), new Enemy(), new Enemy()];
 // Place the player object in a variable called player
-
-
-
+const player = new Player();
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
